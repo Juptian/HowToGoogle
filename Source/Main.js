8b6465881665bf0330e81448ff5b2349e19f686e
Juptian <juptianraffles@gmail.com>
2021-03-26 18:59:10 -0400

    Added two images for using quotations vs not using quotations, Edited the JavaScript file to make it a little easier to read Added some elements to the HTML and CSS scripts

--- Source/Main.js
+++ Source/Main.js
@@ -1,12 +1,19 @@
+//Toggling between light and dark mode!
 let light_mode_toggle = () => {
+    //Toggling a class to let the CSS do the heavy lifting
     let element = document.body;
-    element.classList.toggle("dark_mode")
+    element.classList.toggle("body")
+
+    //Changing the actual toggle itself 
     let toggle = document.getElementById("LMT")
-    if(toggle.innerText == "Dark mode toggle!") { 
+    if(toggle.innerText == "Dark mode toggle!") 
+    { 
         toggle.innerText = "Light mode toggle!"
         toggle.style.backgroundColor = "whitesmoke"
         toggle.style.color = "black"
-    } else {
+    } 
+    else 
+    {
         toggle.innerText = "Dark mode toggle!"
         toggle.style.backgroundColor = "black"
         toggle.style.color = "whitesmoke"
